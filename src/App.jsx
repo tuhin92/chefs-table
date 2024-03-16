3c8dbadd652d4d42b58a3a1aa7784d6099c7b252
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-16 13:40:22 +0600

    Add navbar with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './component/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-6xl'>Recipe </h1>
+      <Navbar></Navbar>
     </>
   )
 }
