6cfe827f851da36ed9afb211ccebe4a5a096ada0
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-16 14:04:36 +0600

    Some modification in navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div className='container mx-auto'>
-            <div class="navbar bg-base-100">
-                <div class="navbar-start">
-                    <div class="dropdown">
-                    <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                    <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         <li><a>Home</a></li>
                         <li><a>Recipes</a></li>
                         <li><a>About</a></li>
                         <li><a>Search</a></li>
                     </ul>
                     </div>
-                    <a class="btn btn-ghost text-3xl font-bold">Recipe Calories</a>
+                    <a className="btn btn-ghost text-3xl font-bold">Recipe Calories</a>
                 </div>
-                <div class="navbar-center hidden lg:flex">
-                    <ul class="menu menu-horizontal px-1">
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
                         <li><a>Home</a></li>
                         <li><a>Recipes</a></li>
                         <li><a>About</a></li>
                         <li><a>Search</a></li>
                     </ul>
                 </div>
-                <div class="navbar-end gap-4">
+                <div className="navbar-end gap-4">
                 <label className="input bg-gray-200 rounded-3xl flex items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
                     <input type="text" className="grow border-none" placeholder="Search" />
